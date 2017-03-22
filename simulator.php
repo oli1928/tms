8adf81ec4e2866635634046455d21b5183dfcd92
Olivier Fischer <olivier.fischer@student.manchester.ac.uk>
2017-03-22 20:22:40 +0000

    Added refreshing of tm list

--- simulator.php
+++ simulator.php
@@ -1,115 +1,146 @@
+<?php
+ob_start();
+session_start();
+$SignedIn;
+?>
 <!DOCTYPE html>
 <!-- Used w3Schools -->
 <html>
 <title>
-  Turing Machine Simulator
+    Turing Machine Simulator
 </title>
+
 <head>
-  <link rel="stylesheet" type="text/css" href="mainstyle.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="mainstyle.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 </head>
 <body>
-  <span class="title-tms">TURING MACHINE SIMULATOR</span>
-
-  <div class="nav-bar">
+<span class="title-tms">TURING MACHINE SIMULATOR</span>
+<h1>
+    <br><br>
+    <?php
+    if(isset($_SESSION['Id'])){
+        echo "Hello id: ". $_SESSION['Id'];
+        $SignedIn = True;
+    } // if
+    else {
+        $_SESSION["uname"] = "default";
+        echo "Not currently signed in";
+        $SignedIn = False;
+    }
+    ?>
+</h1>
+<div class="nav-bar">
     <ul>
-      <li><a class="active" href="#home">Home</a></li>
-      <li><a href="sqlCheck.php">Discover</a></li>
-      <li><a href="simulator.php">Simulator</a></li>
-      <li><a href="#someLink2">someLink2</a></li>
-      <span class="loginbutton"><li><button onclick="document.getElementById('id01').style.display='block'" class="loginbutton">Login</button></li></span>
-      <span class="signupbutton"><li><button onclick="document.getElementById('id02').style.display='block'" style="width:auto;">Sign Up</button></li></span>
+        <li><a class="active" href="index.php">Home</a></li>
+        <li><a href="Discover.php">Discover</a></li>
+        <li><a href="Simulator.php">Simulator</a></li>
+        <li><a href="myMachines.php">my Machines</a></li>
+        <?php if(!(isset($_SESSION['Id']))) { ?>
+            <span class="loginbutton"><li><button onclick="document.getElementById('id01').style.display='block'" class="loginbutton">Login</button></li></span>
+            <span class="signupbutton"><li><button onclick="document.getElementById('id02').style.display='block'" style="width:auto;">Sign Up</button></li></span>
+        <?php } ?>
+        <?php if(isset($_SESSION['Id'])) { ?>
+            <form action="LogOut.php" method="post">
+                <span class="logoutbutton"><li><button class="logoutbutton" type="submit" >Logout</button></li></span>
+            </form>
+        <?php } ?>
     </ul>
-  </div>
+</div>
 
 <!-- Log in button -->
+
 <div id="id01" class="modal">
 
-  <form class="modal-content animate" action="/login.php">
-    <div class="imgcontainer">
-      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
-      <img src="img_avatar2.png" alt="Avatar" class="avatar">
-    </div>
+    <form class="modal-content animate" action = "login.php" method = "post">
+        <div class="imgcontainer">
+            <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+            <img src="img_avatar2.png" alt="Avatar" class="avatar">
+        </div>
 
-    <div class="container">
+        <div class="container">
 
+            <label><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="uname" required>
 
+            <label><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="psw" required>
 
-      <label><b>Username</b></label>
-      <input type="text" placeholder="Enter Username" name="uname" required>
+            <button type="submit">Login</button>
+            <input type="checkbox" checked="checked"> Remember me
+        </div>
 
-      <label><b>Password</b></label>
-      <input type="password" placeholder="Enter Password" name="psw" required>
-
-      <button type="submit">Login</button>
-      <input type="checkbox" checked="checked"> Remember me
-    </div>
-
-    <div class="container" style="background-color:#f1f1f1">
-      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-      <span class="psw">Forgot <a href="#">password?</a></span>
-    </div>
-  </form>
+        <div class="container" style="background-color:#f1f1f1">
+            <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+            <span class="psw">Forgot <a href="forgotPSWD.php">password?</a></span>
+        </div>
+    </form>
 </div>
 
 <script>
-// Get the modal
-var modal = document.getElementById('id01');
+    // Get the modal
+    var modal = document.getElementById('id01');
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
     }
-}
 </script>
 <!-- Sign up button -->
 
 
 <div id="id02" class="modal">
-  <span onclick="document.getElementById('id02').style.display='none'" class="closesignup" title="Close Modal">×</span>
-  <form class="modal-content animate" action="/signup.php">
-    <div class="container">
-      <label><b>Email</b></label>
-      <input type="text" placeholder="Enter Email" name="email" required>
+    <span onclick="document.getElementById('id02').style.display='none'" class="closesignup" title="Close Modal">×</span>
+    <form class="modal-content animate" action="signup.php" method = "post">
+        <div class="container">
+            <label><b>Name</b></label>
+            <input type="text" placeholder="Enter your name" name="name" required>
 
-      <label><b>Password</b></label>
-      <input type="password" placeholder="Enter Password" name="psw" required>
+            <label><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="uname" required>
+            <label><b>Email</b></label>
+            <input type="text" placeholder="Enter Email" name="email" required>
 
-      <label><b>Repeat Password</b></label>
-      <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
-      <input type="checkbox" checked="checked"> Remember me
-      <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
+            <label><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="psw" required>
 
-      <div class="clearfix">
-        <button type="button" onclick="document.getElementById('id02').style.display='none'" class="cancelsignupbtn">Cancel</button>
-        <button type="submit" class="signupbtn">Sign Up</button>
-      </div>
-    </div>
-  </form>
+            <label><b>Repeat Password</b></label>
+            <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+            <input type="checkbox" checked="checked"> Remember me
+            <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
+
+            <div class="clearfix">
+                <button type="button" onclick="document.getElementById('id02').style.display='none'" class="cancelsignupbtn">Cancel</button>
+                <button type="submit" class="signupbtn">Sign Up</button>
+            </div>
+        </div>
+    </form>
 </div>
 
 <script>
-// Get the modal
-var modal = document.getElementById('id01');
-var modal2 = document.getElementById('id02');
+    // Get the modal
+    var modal1 = document.getElementById('id01');
+    var modal2 = document.getElementById('id02');
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
-window.onclick = function(event) {
-    if (event.target == modal2) {
-        modal2.style.display = "none";
+    // When the user clicks anywhere outside of the modal, close it
+
+    window.onclick = function(event) {
+        if (event.target == modal2) {
+            modal2.style.display = "none";
+        }
     }
-}
+
+    window.onclick = function(event) {
+        if (event.target == modal1) {
+            modal1.style.display = "none";
+        }
+    }
+</script>
 </script>
 
   <?php
-  session_start();
-  $_SESSION['Id'] = 1;
 
   class Tape
   {
@@ -511,7 +542,7 @@ window.onclick = function(event) {
 
 //make a variable the sql commands to pass into the database
       $input_data = "INSERT INTO TM(Title, Description, isPublic, TMCode, AuthourId)
-VALUES('$Title','$Description','$isPublic','$TMCode','$AuthourId')";
+VALUES('$Title','$Description','$isPublic','$TMCode', $AuthourId)";
 //All the valid variables nested so it will only try if all fields are valid
       if($title_valid){
           if($description_valid){
@@ -530,6 +561,11 @@ VALUES('$Title','$Description','$isPublic','$TMCode','$AuthourId')";
 
 
       $connection->close();
+
+      echo '<script type="text/javascript">',
+      'updateTMList();',
+      '</script>'
+      ;
 
   }
 
