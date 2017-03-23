e48a26cb594a19e5467ba01992e0fee2a059e570
Olivier Fischer <olivier.fischer@student.manchester.ac.uk>
2017-03-23 13:10:18 +0000

    Part of validator

---
+++ validation/Require.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once ("Model/MoveInstruction.php");
+require_once ("Model/Rule.php");
+require_once ("Model/State.php");
+require_once ("Model/Symbol.php");
+require_once ("Model/Tape.php");
+require_once ('Model/TuringModel.php');
+
+require_once ("Services/ConsistencyValidator.php");
+require_once ("Services/OutputFormatter.php");
+require_once ("Services/ErrorHandler.php");
+require_once ("Services/Lexer.php");
+require_once ("Services/Parser.php");
+
